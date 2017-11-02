bb539bc1584c5866b855e3ff23372c9d9eb532a4
rahul.bajracharya <rahul.bajracharya@spi.com>
2017-11-02 18:09:13 +0545

    mongo commands added to file

--- mongo_data/initi_replica.js
+++ mongo_data/initi_replica.js
@@ -1,9 +1,23 @@
 config = {
-    _id:"test", members:[
-        {_id:0, host:"localhost:27017"},
-        {_id:1, host:"localhost:27018"},
-        {_id:2, host:"localhost:27019"}
+    _id:"testing", members:[
+        {_id:0, host:"localhost:27018"},
+        {_id:1, host:"localhost:27019"},
+        {_id:2, host:"localhost:27020"}
     ]
 }
 rs.initiate(config);
 rs.status();
+
+
+
+sc.exe create MongoDB27018 binPath= "\"C:\Program Files\MongoDB\Server\3.4\bin\mongod.exe\" --service --config=\"C:\data\rs1\rs1.cfg\"" DisplayName= "MongoDB-27018" start= "auto"
+
+sc.exe create MongoDB27019 binPath= "\"C:\Program Files\MongoDB\Server\3.4\bin\mongod.exe\" --service --config=\"C:\data\rs2\rs2.cfg\"" DisplayName= "MongoDB-27019" start= "auto"
+sc.exe create MongoDB27020 binPath= "\"C:\Program Files\MongoDB\Server\3.4\bin\mongod.exe\" --service --config=\"C:\data\rs3\rs3.cfg\"" DisplayName= "MongoDB-27020" start= "auto"
+
+
+
+Initiate the replica set from within the mongo shell.
+
+rs.initiate()
+The initial replica set configuration can be verified using rs.conf().
